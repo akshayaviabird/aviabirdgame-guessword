02eab4ceea1a1a17563447743342c519e6746c11
Rohit171994 <rohitkumarpoddar1917@gmail.com>
2021-09-17 12:19:12 +0530

    changes

--- public/js/data.js
+++ public/js/data.js
@@ -1,18 +1,47 @@
-let arrData=[]
+let arrData = []
 
 fetch('https://immense-dusk-54293.herokuapp.com/api/v1/guessimage')
-.then((response)=>response.json())
-.then((result)=>{
+    .then((response) => response.json())
+    .then((result) => {
 
-    arrData.push(result.data[0].data)
-    console.log("ssss",arrData[0])
-}).catch((err)=>{
-    console.log(err)
+        arrData.push(result.data[0].data)
+    }).catch((err) => {
+        console.log(err)
+    })
+
+let btn = document.getElementById('btnclick')
+let getImage = document.getElementById('displayImage')
+let getText = document.getElementById('test-guess')
+let dataID = 1
+
+btn.addEventListener('click', () => {
+    setInterval(function () {
+
+        let arr = []
+        dataID = dataID + 1
+        arr = arrData[0].find(item => item.id == dataID)
+        getImage.src = arr[0].url
+        getImage.innerHTML = arr[0].word.replace(str.substring(1, arr[0].word.length - 1), "*******");
+    }, 3000);
+    var timeleft = 5;
+    var downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+        }
+        document.getElementById("progressBar").value = 10 - timeleft;
+        timeleft -= 1;
+        document.getElementById("rohit").innerHTML = timeleft + 1;
+        if (timeleft + 1 == 0) {
+            document.getElementById("seconds").innerHTML = "Time's Up";
+        }
+        else {
+            document.getElementById("seconds").innerHTML = "seconds Left";
+        }
+
+    }, 1000);
+
+
 })
 
-let btn=document.getElementById('btnclick')
-let getImage=document.getElementById('displayImage')
-btn.addEventListener('click',()=>{
-getImage.src=arrData[0] 
 
-})
+
