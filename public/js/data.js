0a755c47151e36046c145b154b120281afd29525
Rohit171994 <rohitkumarpoddar1917@gmail.com>
2021-09-17 13:55:18 +0530

    updated changes

--- public/js/data.js
+++ public/js/data.js
@@ -16,28 +16,48 @@ let dataID = 0
 
 btn.addEventListener('click', () => {
     // setInterval(function () {
-        let arr = []
-        dataID = dataID + 1
-        arr = arrData[0].find(item => item.id == dataID)
+    let arr = []
+    dataID = dataID + 1
+    arr = arrData[0].find(item => item.id == dataID)
+    if (arr == undefined) {
+        getImage.src = "https://i.pinimg.com/736x/34/2c/53/342c53cd4d6c8ae99097e19e4b44f2cf.jpg"
+        document.getElementById("rohit").style.display = "none"
+        document.getElementById("seconds").style.display = "none"
+        document.getElementById("progressBar").style.display = "none"
+        document.getElementById('test-guess').style.display = "none"
+    }
+    else {
+
         getImage.src = arr.url
         getText.innerHTML = arr.word.replace(arr.word.substring(1, arr.word.length - 1), "*******");
         btnclick
-        document.getElementById('btnclick').innerText="next"
+        document.getElementById('btnclick').innerText = "next"
+    }
     // }, 10000);
 
-    var timeleft = 10;
+    var timeleft = 3;
     var downloadTimer = setInterval(function () {
         if (timeleft <= 0) {
             clearInterval(downloadTimer);
         }
-        document.getElementById("progressBar").value = 10 - timeleft;
+        document.getElementById("progressBar").value = 3 - timeleft;
         timeleft -= 1;
         document.getElementById("rohit").innerHTML = timeleft + 1;
         if (timeleft + 1 == 0) {
             document.getElementById("seconds").innerHTML = "Time's Up";
+            document.getElementById('btnclick').innerText = "next"
+            if (arr == undefined) {
+                document.getElementById('btnclick').style.display = "none"
+            }
+            else {
+                document.getElementById('btnclick').style.display = "block"
+            }
+
         }
         else {
             document.getElementById("seconds").innerHTML = "seconds Left";
+            document.getElementById('btnclick').innerText = "next"
+            document.getElementById('btnclick').style.display = "none"
         }
 
     }, 1000);
